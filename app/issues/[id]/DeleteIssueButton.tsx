af3d376f95917b40ec2cd99f26515d965a7d5ccc
AkshayK2026 <akshaykadam.7096@gmail.com>
2025-05-05 09:30:53 +0530

    delete

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,12 +2,14 @@
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
   const router=useRouter();
+  const[error,setError]= useState(false);
 
   return (
+    <>
     <AlertDialog.Root>
 		<AlertDialog.Trigger>
     <Button color='red'>Delete Issue</Button>
@@ -18,17 +20,36 @@ const DeleteIssueButton = ({issueId}:{issueId:number}) => {
     <Flex mt='4' gap='3'>
     <AlertDialog.Cancel><Button variant='soft' color='gray' >Cancel</Button></AlertDialog.Cancel>
     <AlertDialog.Action><Button color='red' onClick={async()=>{
-      await axios.delete('/api/issues/' + issueId) ;
-      router.push('/issues');
-      router.refresh();}}>
+      try {
+        await axios.delete('/api/issues/' + issueId) ;
+       router.push('/issues');
+       router.refresh();
+        
+      } catch (error) {
+        setError(true);
+        
+      }
+      }}>
       Delete issue</Button></AlertDialog.Action>
      
 
     </Flex>
     </AlertDialog.Content>
     </AlertDialog.Root>
+    <AlertDialog.Root>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Error</AlertDialog.Title>
+        <AlertDialog.Description>This  issue error could be deleted</AlertDialog.Description>
+    
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+    </>
    
   )
 }
 
 export default DeleteIssueButton
+
+// function useState(arg0: boolean): [any, any] {
+//   throw new Error('Function not implemented.')
+// }
