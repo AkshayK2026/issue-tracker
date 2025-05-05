e7d1153aa1c00f28e4c73f519b2cf10cdce7ef27
AkshayK2026 <akshaykadam.7096@gmail.com>
2025-05-05 11:07:16 +0530

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -7,6 +7,18 @@ import React, { useState } from 'react'
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
   const router=useRouter();
   const[error,setError]= useState(false);
+  const deleteissue=async()=>{
+    try {
+    
+      await axios.delete('/api/issues/' + issueId) ;
+     router.push('/issues');
+     router.refresh();
+      
+    } catch (error) {
+      setError(true);
+      
+    }
+    }
 
   return (
     <>
@@ -19,27 +31,18 @@ const DeleteIssueButton = ({issueId}:{issueId:number}) => {
     <AlertDialog.Description> Are you Sure to  Delete ? This Action can't be Undone</AlertDialog.Description>
     <Flex mt='4' gap='3'>
     <AlertDialog.Cancel><Button variant='soft' color='gray' >Cancel</Button></AlertDialog.Cancel>
-    <AlertDialog.Action><Button color='red' onClick={async()=>{
-      try {
-        await axios.delete('/api/issues/' + issueId) ;
-       router.push('/issues');
-       router.refresh();
-        
-      } catch (error) {
-        setError(true);
-        
-      }
-      }}>
+    <AlertDialog.Action><Button color='red' onClick={deleteissue}>
       Delete issue</Button></AlertDialog.Action>
      
 
     </Flex>
     </AlertDialog.Content>
     </AlertDialog.Root>
-    <AlertDialog.Root>
+    <AlertDialog.Root open={error}>
       <AlertDialog.Content>
         <AlertDialog.Title>Error</AlertDialog.Title>
         <AlertDialog.Description>This  issue error could be deleted</AlertDialog.Description>
+        <Button color='gray' variant='soft' mt='3' onClick={()=>setError(false)}>Ok</Button>
     
       </AlertDialog.Content>
     </AlertDialog.Root>
